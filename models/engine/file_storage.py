f6bd37e39fa90c2c0a4ba3c78629eae913f07f7f
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-13 15:00:27 +0530

    Update file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+""" The class FileStorage contains data serialization methods.
+
+Attributes:
+    file_path (str): Defines the file path to the JSON FileStorage.
+    __objects (dict): A dictionary that contains objects stored in FileStorage.
+
+Methods:
+    all(): Returns the __objects dictionary.
+    new(obj): Sets in __objects the obj with key <obj class name>.id.
+    save(): Serializes __objects to the JSON file (path: __file_path).
+    reload(): Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists).
+
+Note:
+    This class is designed for handling data serialization and deserialization.
+"""
+
+class FileStorage:
+    file_path = "file.json"
+    __objects = {}
+    
+    def __init__(self):
+        pass
+
+    def all(self):
+        """Returns the __objects dictionary."""
+        return FileStorage.__objects
+        
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        self.temp_dict = obj.to_dict()
+        self.class_name = self.temp_dict.pop('__class__', None)
+        self.key = f"{self.class_name}.{str(obj.id)}"
+        FileStorage.__objects[self.key] = self.temp_dict
+        return FileStorage.__objects
+        
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        __objects_json = json.dumps(FileStorage.__objects)
+        with open(FileStorage.file_path, 'w') as file:
+            file.write(__objects_json)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)."""
+        if FileStorage.file_path:
+            with open(FileStorage.file_path, 'r') as file:
+                data = file.read()
+            FileStorage.__objects = json.loads(data)
