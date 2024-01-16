327ccc0fccd7880825cb05127ceab97a27ec7938
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-16 14:41:40 +0530

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 import json
-# from models import storage
+from models import storage
 
 """The Basemodel class defines a basic model structure.
 
@@ -41,7 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             
-            # storage.new()
+            storage.new()
         
     def updated_at(self):
         """Return the current value of the 'updated_at' attribute."""
@@ -50,7 +50,7 @@ class BaseModel:
     def save(self):
         """Update the 'updated_at' attribute to the current timestamp."""
         self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        # storage.save()
+        storage.save()
 
     def to_dict(self):
         """Convert the object to a dictionary for serialization."""
