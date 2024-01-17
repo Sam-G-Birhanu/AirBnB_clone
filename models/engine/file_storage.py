e9853e2dee06b5d93ed314f08b5554b8499e8a07
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-18 03:04:29 +0530

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,10 +42,10 @@ class FileStorage:
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         new_dict = copy.deepcopy(FileStorage.__objects)
-        for obj in new_dict:
+        for obj in new_dict.values():
             print(obj)
-            obj.updated_at = obj.updated_at.isoformat()
-            obj.created_at = obj.created_at.isoformat()
+            obj['updated_at'] = obj['updated_at'].isoformat()
+            obj['created_at'] = obj['created_at'].isoformat()
         __objects_json = json.dumps(new_dict)
         with open(FileStorage.__file_path, 'w') as file:
             file.write(__objects_json)
