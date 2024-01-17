d54965a66b098227bae782a55bb50f7407542617
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-18 02:18:05 +0530

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -57,7 +57,7 @@ class BaseModel:
         inst_dict = {'__class__': 'BaseModel'}
         inst_dict.update(self.__dict__.copy())
         inst_dict['created_at'] = self.created_at.isoformat()
-        inst_dict['updated_at'] = self._updated_at.isoformat()
+        inst_dict['updated_at'] = self.updated_at.isoformat()
         return inst_dict
 
     def __str__(self):
