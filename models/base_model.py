3baa4fd030d138cd4ec5489c79a3521585fa709b
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-17 19:40:12 +0530

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -37,9 +37,11 @@ class BaseModel:
         else:
             self.my_number = ""
             self.name = ""
-            self.updated_at = datetime.now()
+            self.updated_at = datetime.now().isoformat()
+            self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            self.created_at = datetime.now().isoformat()
+            self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
             
             storage.new(self)
         
