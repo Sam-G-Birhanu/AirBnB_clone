92253ffe0fbb42f7acf1f7836d278697ea2993ad
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-18 18:29:34 +0530

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -51,6 +51,8 @@ class BaseModel:
         """Update the 'updated_at' attribute to the current timestamp."""
         # self.updated_at = datetime.now().isoformat()
         # self.created_at = datetime.now().isoformat()
+        if type(self) != dict:
+            self = self.__dict__
         storage.save()
 
     def to_dict(self):
