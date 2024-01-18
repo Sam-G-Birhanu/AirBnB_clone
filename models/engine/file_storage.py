ce5ca904691c6b2aaca8f2dae1432b3f421998be
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-18 18:03:21 +0530

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,11 +50,12 @@ class FileStorage:
             # print("This is type of new_dict in save")
             # print(type(new_dict))
             for obj in new_dict.values():
-                print(type(obj))
+                # print(type(obj))
                 if type(obj) != dict:
                     obj = obj.__dict__
                 obj['updated_at'] = obj['updated_at'].isoformat()
                 obj['created_at'] = obj['created_at'].isoformat()
+        print(type(new_dict))
         __objects_json = json.dumps(new_dict)
         with open(FileStorage.__file_path, 'w') as file:
             file.write(__objects_json)
