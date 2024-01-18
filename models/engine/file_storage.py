8e6ef0f2103f7748ac8680f26c41320f261aec79
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-18 18:13:14 +0530

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -80,7 +80,7 @@ class FileStorage:
                     ####
                     # print("I'm Value")
                     # print(value)
-                    FileStorage.__objects[key] = instance
+                    FileStorage.__objects[key] = instance.__dict__
                     # print("I'm instance")
                     # print(instance)
             # # FileStorage.__objects = BaseModel(my_dict)
