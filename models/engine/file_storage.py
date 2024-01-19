b3fbe93ff2fff1f90d0fec8658eeeb6aa272cb22
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-20 02:52:28 +0530

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,57 +29,52 @@ class FileStorage:
 
     def all(self):
         """Returns the __objects dictionary."""
-        new_obj = copy.deepcopy(FileStorage.__objects)
-        for obj in new_obj.values():
-            obj_copy = copy.deepcopy(obj)
-            if type(obj) != dict:
-                self.temp_dict = obj.__dict__
-            else:
-                self.temp_dict = obj
-            self.class_name = self.temp_dict.pop('__class__', None)
-            self.key = f"{self.class_name}.{str(self.temp_dict['id'])}"
-            if type(obj_copy) != dict:
-                FileStorage.__objects[self.key] = obj_copy.__dict__
-            else:
-                FileStorage.__objects[self.key] = obj_copy
         return FileStorage.__objects
         
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
-        obj_copy = copy.deepcopy(obj)
-        self.temp_dict = obj.to_dict()
-        self.class_name = self.temp_dict.pop('__class__', None)
-        self.key = f"{self.class_name}.{str(obj.id)}"
-        # obj_copy.updated_at = obj_copy.updated_at.isoformat()
-        # obj_copy.created_at = obj_copy.created_at.isoformat()
-        FileStorage.__objects[self.key] = obj_copy
-        # print(obj_copy.__dict__)
-        print("in new method")
-        print(FileStorage.__objects)
-        print("in new method")
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+        
+        # obj_copy = copy.deepcopy(obj)
+        # self.temp_dict = obj.to_dict()
+        # self.class_name = self.temp_dict.pop('__class__', None)
+        # self.key = f"{self.class_name}.{str(obj.id)}"
+        # # obj_copy.updated_at = obj_copy.updated_at.isoformat()
+        # # obj_copy.created_at = obj_copy.created_at.isoformat()
+        # FileStorage.__objects[self.key] = obj_copy
+        # # print(obj_copy.__dict__)
+        # print("in new method")
+        # print(FileStorage.__objects)
+        # print("in new method")
         
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
-        new_dict = copy.deepcopy(FileStorage.__objects)
-        print("in save method")
-        print(FileStorage.__objects)
-        print("in save method")
-        if type(new_dict) != dict:
-            new_dict = new_dict.__dict__
-        # else:
-        #     # print("This is type of new_dict in save")
-        #     # print(type(new_dict))
-        for key,obj in new_dict.items():
-            # print(type(obj))
-            if type(obj) != dict:
-                # print("i'm in")
-                new_dict[key] = obj.__dict__
-                # print(type(obj))
-                # print(obj)
-            new_dict[key]['updated_at'] = new_dict[key]['updated_at'].isoformat()
-            new_dict[key]['created_at'] = new_dict[key]['created_at'].isoformat()
-        # print(new_dict)
-        __objects_json = json.dumps(new_dict)
+        ser_dict = copy.deepcopy(FileStorage.__objects)
+        for key, obj in serialized_dict.items():
+            new_obj = obj.to_dict()
+            ser_dict[key] = new_obj
+        
+        # new_dict = copy.deepcopy(FileStorage.__objects)
+        # print("in save method")
+        # print(FileStorage.__objects)
+        # print("in save method")
+        # if type(new_dict) != dict:
+        #     new_dict = new_dict.__dict__
+        # # else:
+        # #     # print("This is type of new_dict in save")
+        # #     # print(type(new_dict))
+        # for key,obj in new_dict.items():
+        #     # print(type(obj))
+        #     if type(obj) != dict:
+        #         # print("i'm in")
+        #         new_dict[key] = obj.__dict__
+        #         # print(type(obj))
+        #         # print(obj)
+        #     new_dict[key]['updated_at'] = new_dict[key]['updated_at'].isoformat()
+        #     new_dict[key]['created_at'] = new_dict[key]['created_at'].isoformat()
+        # # print(new_dict)
+        __objects_json = json.dumps(ser_dict)
         with open(FileStorage.__file_path, 'w') as file:
             file.write(__objects_json)
 
@@ -89,26 +84,33 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as file:
                 data = file.read()
                 deserialized_data = json.loads(data)
-                # FileStorage.__objects = deserialized_data
-                for key, value in deserialized_data.items():
-                    # class_name, obj_id = key.split('.')
+                for key, dict_obj in deserialized_data.items():
                     module_name = 'models.base_model'
                     module = importlib.import_module(module_name)
                     class_ = getattr(module, 'BaseModel')
-                    instance = class_(**value)
-                    ####
-                    instance.updated_at = datetime.strptime(instance.updated_at.isoformat(),"%Y-%m-%dT%H:%M:%S.%f")
-                    instance.created_at = datetime.strptime(instance.created_at.isoformat(),"%Y-%m-%dT%H:%M:%S.%f")
-                    ####
-                    print("I'm key")
-                    print(key)
-                    print(instance)
+                    instance = class_(**dict_obj)
                     FileStorage.__objects[key] = instance
-                    # print(FileStorage.__objects[key])
-                    # print(instance)
-            print("in reload method")
-            print(FileStorage.__objects)
-            print("in reload method")
+                
+            #     # FileStorage.__objects = deserialized_data
+            #     for key, value in deserialized_data.items():
+            #         # class_name, obj_id = key.split('.')
+            #         module_name = 'models.base_model'
+            #         module = importlib.import_module(module_name)
+            #         class_ = getattr(module, 'BaseModel')
+            #         instance = class_(**value)
+            #         ####
+            #         instance.updated_at = datetime.strptime(instance.updated_at.isoformat(),"%Y-%m-%dT%H:%M:%S.%f")
+            #         instance.created_at = datetime.strptime(instance.created_at.isoformat(),"%Y-%m-%dT%H:%M:%S.%f")
+            #         ####
+            #         print("I'm key")
+            #         print(key)
+            #         print(instance)
+            #         FileStorage.__objects[key] = instance
+            #         # print(FileStorage.__objects[key])
+            #         # print(instance)
+            # print("in reload method")
+            # print(FileStorage.__objects)
+            # print("in reload method")
             
         
             # # FileStorage.__objects = BaseModel(my_dict)
