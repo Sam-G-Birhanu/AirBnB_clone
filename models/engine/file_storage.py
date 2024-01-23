693071a5999303ced96476ab881b3a8eebcb503c
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-23 22:52:38 +0530

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,18 +40,6 @@ class FileStorage:
         else:
             pass
         
-        # obj_copy = copy.deepcopy(obj)
-        # self.temp_dict = obj.to_dict()
-        # self.class_name = self.temp_dict.pop('__class__', None)
-        # self.key = f"{self.class_name}.{str(obj.id)}"
-        # # obj_copy.updated_at = obj_copy.updated_at.isoformat()
-        # # obj_copy.created_at = obj_copy.created_at.isoformat()
-        # FileStorage.__objects[self.key] = obj_copy
-        # # print(obj_copy.__dict__)
-        # print("in new method")
-        # print(FileStorage.__objects)
-        # print("in new method")
-        
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         if FileStorage.__file_path != None:
@@ -62,7 +50,6 @@ class FileStorage:
             __objects_json = json.dumps(ser_dict)
             with open(FileStorage.__file_path, 'w') as file:
                 file.write(__objects_json)
-                print('success')
         else:
             pass
 
