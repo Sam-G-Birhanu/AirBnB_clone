4fe9e50104a9b3b720b61124333e500d1aceb24e
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-01-24 22:08:06 -0800

    dos2unix

--- models/review.py
+++ models/review.py
@@ -2,9 +2,9 @@
 from models.base_model import BaseModel
 
 class Review(BaseModel):
-  place_id = ""
-  user_id = ""
-  text = ""
-  
-   def __init__(self):
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self):
     pass
