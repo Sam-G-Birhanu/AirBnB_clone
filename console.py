2453e91cf0a9a098c0712d5247452d50aacc4f1f
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-25 12:03:54 +0530

    Update console.py

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ from models.engine.file_storage import FileStorage
 from models import storage
 import re
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 class HBNBCommand(cmd.Cmd):
     """The HBNBCommand class defines a command-line interpreter for the HBNB project.
 
